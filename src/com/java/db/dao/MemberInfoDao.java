66336f8f0db9998f87caa4e47af5b8157dff10d1
ByungJinYoo <ppwm111@naver.com>
2021-05-16 23:15:08 +0900

    05/16 병진 회원정보수정, INDEX에 가게정보 뿌리기

--- src/com/java/db/dao/MemberInfoDao.java
+++ src/com/java/db/dao/MemberInfoDao.java
@@ -80,8 +80,43 @@ public class MemberInfoDao {
         }
 		return null;
 
-
-
 	}
 
+	
+	//비밀번호 변경
+	public int update(String id, String pw) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+        String query = "UPDATE member_info SET member_pw = ? WHERE member_id = ?";
+
+
+        try {
+        	con = DriverManager.getConnection(url,uid,pwd);
+            pstmt = con.prepareStatement(query);
+            
+            pstmt.setString(1,pw);
+            pstmt.setString(2,id);
+            
+            return pstmt.executeUpdate();
+ 
+        }catch(Exception e) {
+
+            e.printStackTrace();   
+
+        }finally {
+        	try {
+        		if (con != null)
+					con.close();
+				if (pstmt != null)
+					pstmt.close();
+				if(rs!=null)
+					rs.close();
+        	}catch(Exception e2) {
+        		e2.printStackTrace();
+        	}
+        }
+		return -1;
+
+	}
 }
--- src/com/java/db/dao/SManagerInfoDao.java
+++ src/com/java/db/dao/SManagerInfoDao.java
@@ -42,8 +42,6 @@ public class SManagerInfoDao {
             
             rs = pstmt.executeQuery();
             
-         
-
             
             if(rs.next()) {
             	String num = rs.getString("shop_num");
@@ -84,5 +82,44 @@ public class SManagerInfoDao {
 
 
 	}
+	
+	
+	
+	//비밀번호 변경
+		public int update(String id, String pw) {
+			Connection con = null;
+			PreparedStatement pstmt = null;
+			
+	        String query = "UPDATE shop_manager SET shop_pw = ? WHERE shop_id = ?";
+
+
+	        try {
+	        	con = DriverManager.getConnection(url,uid,pwd);
+	            pstmt = con.prepareStatement(query);
+	            
+	            pstmt.setString(1,pw);
+	            pstmt.setString(2,id);
+	            
+	            return pstmt.executeUpdate();
+	 
+	        }catch(Exception e) {
+
+	            e.printStackTrace();   
+
+	        }finally {
+	        	try {
+	        		if (con != null)
+						con.close();
+					if (pstmt != null)
+						pstmt.close();
+					if(rs!=null)
+						rs.close();
+	        	}catch(Exception e2) {
+	        		e2.printStackTrace();
+	        	}
+	        }
+			return -1;
+
+		}
 
 }
